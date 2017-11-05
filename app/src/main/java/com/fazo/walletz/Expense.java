000c4dd3024cb571e17f732f0b1388c8db44a8df
Hussain Fazaal <Fazo@Hussains-MacBook-Pro.local>
2017-11-05 17:30:06 +0800

    Initial commit has the main navigation drawer items connected with their respective blank fragments without any logic added yet.

---
+++ app/src/main/java/com/fazo/walletz/Expense.java
@@ -0,0 +1,54 @@
+package com.fazo.walletz;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Expense extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public Expense() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction("Expense");
+        }
+        return inflater.inflate(R.layout.fragment_expense, container, false);
+    }
+
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(String title);
+    }
+}
---
+++ app/src/main/java/com/fazo/walletz/Income.java
@@ -0,0 +1,53 @@
+package com.fazo.walletz;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Income extends Fragment {
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public Income() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        if (mListener != null) {
+            mListener.onFragmentInteraction("Income");
+        }
+        return inflater.inflate(R.layout.fragment_income, container, false);
+    }
+
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(String title);
+    }
+}
